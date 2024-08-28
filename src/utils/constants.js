768d9fd4be45efd8244f34981e2602608a161d3b
Lokesh Chintala <80199959+lokesh-02@users.noreply.github.com>
2024-08-28 12:02:14 +0530

    search results error fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const GOOGLE_API_KEY='AIzaSyAfwCWSjLrszciwUrmWv9fDOsfWm8msEoI'
 export const YOUTUBE_VIDEO_API=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
 
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 
 export const YOUTUBE_SEARCH_RESULTS_API=`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${GOOGLE_API_KEY}&q=`;
